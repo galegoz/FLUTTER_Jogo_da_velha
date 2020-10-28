26a766b564ce045d47e9cb3b720ed7028ee41801
Lucas Carvalho <llucassduarte50@gmail.com>
2020-10-28 19:34:03 -0300

    Update game_controller.dart
    Regra de negócio

--- lib/constrollers/game_controller.dart
+++ lib/constrollers/game_controller.dart
@@ -27,4 +27,29 @@ class GameController{
     _initialize();
   }
 
+  void markBoardTileByIndex(index){
+    final tile = tiles[index];
+    if(currentPlayer == PlayerType.player1){
+      _markBoardTileWithPlayer1(tile);
+    }else{
+      _markBoardTileWithPlayer2(tile);
+    }
+      tile.enable = false;
+    }
+  
+
+  void _markBoardTileWithPlayer1(BoardTile tile){
+    tile.symbol = PLAYER1_SYMBOL;
+    tile.color = PLAYER1;
+    movesPlayer1.add(tile.id);
+    currentPlayer = PlayerType.player2;
+  }
+
+  void _markBoardTileWithPlayer2(BoardTile tile){
+    tile.symbol = PLAYER1_SYMBOL;
+    tile.color = PLAYER1;
+    movesPlayer2.add(tile.id);
+    currentPlayer = PlayerType.player1;
+  }
+
 }
