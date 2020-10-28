ee2004f48d21b9cf1eb69401e18e8d3d7955fafc
Lucas Carvalho <llucassduarte50@gmail.com>
2020-10-28 20:19:56 -0300

    Create custom_dialog.dart
    Tela de exibição de mensagens para o usuario

---
+++ lib/widgets/custom_dialog.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:jogo_velha/core/constants.dart';
+
+class CustomDialog extends StatelessWidget{
+
+  final String title;
+  final String message;
+  final Function onPressed;
+
+  const CustomDialog({
+    this.title,
+    this.message,
+    this.onPressed,
+  });
+
+
+
+  @override
+  Widget build(BuildContext context){
+    return AlertDialog(
+      title: Text(title),
+      content: Text(message),
+      actions: [
+        FlatButton(
+          child: Text(RESET_BUTTON_LABEL),
+          onPressed: (){
+            Navigator.pop(context);
+            onPressed();
+          },
+        ),
+      ],
+    );
+  }
+}
