5b6deb5985bf64e539b3f4c37e1b12aee93d7078
Lucas Carvalho <llucassduarte50@gmail.com>
2020-10-28 20:05:31 -0300

    Update game_controller.dart
    Checar movimentos, verificar quem ganhou e movimentos aleatorios para o bot

--- lib/constrollers/game_controller.dart
+++ lib/constrollers/game_controller.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:jogo_velha/core/constants.dart';
 import 'package:jogo_velha/enums/player_type.dart';
+import 'package:jogo_velha/enums/winner_type.dart';
 import 'package:jogo_velha/models/board_tile.dart';
 
 class GameController{
@@ -52,4 +55,55 @@ class GameController{
     currentPlayer = PlayerType.player1;
   }
 
+  bool _checkPlayerWinner(List<int> moves){
+    //verificando as linhas
+    if (moves.contains(1) && moves.contains(2) && moves.contains(3)){
+      return true;
+    }
+    if (moves.contains(4) && moves.contains(5) && moves.contains(6)){
+      return true;
+    }
+    if (moves.contains(7) && moves.contains(8) && moves.contains(9)){
+      return true;
+    }
+    //verificando as colunas
+    if (moves.contains(1) && moves.contains(4) && moves.contains(7)){
+      return true;
+    }
+    if (moves.contains(2) && moves.contains(5) && moves.contains(8)){
+      return true;
+    }
+    if (moves.contains(3) && moves.contains(6) && moves.contains(9)){
+      return true;
+    }
+    //verificando diagonais
+    if (moves.contains(1) && moves.contains(5) && moves.contains(9)){
+      return true;
+    }
+    if (moves.contains(3) && moves.contains(5) && moves.contains(7)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  //Verificar vencedor
+  WinnerType checkWinner(){
+    if(_checkPlayerWinner(movesPlayer1)) return WinnerType.player1;
+    if(_checkPlayerWinner(movesPlayer2)) return WinnerType.player2;
+    return WinnerType.none;
+  }
+
+  //Movimento aleatório do bot
+  int movimentoAutomatico(){
+  var list = new List.generate(9, (i) => i + 1);
+  list.removeWhere((element) => movesPlayer1.contains(element));
+  list.removeWhere((element) => movesPlayer2.contains(element));
+
+  var random = new Random();
+  var index = random.nextInt(list.length - 1);
+  return tiles.indexWhere((tile) => tile.id == list[index]);
+
+  }
 }
